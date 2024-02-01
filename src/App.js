c2250e083e9d48a18ab4f865745f5c55b0881c3a
yuvanesh20 <yuvaneshp6@gmail.com>
2024-02-01 21:07:10 +0530

    commit_first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+export default function App() {
+  //props
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Details
+        access="✔"
+        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg"
+        name="sanjay"
+        batch="B43WD"
+      />
+
+      <Details
+        access="❌"
+        img="https://iheartcraftythings.com/wp-content/uploads/2021/04/How-to-Draw-Cartoon-Face-%E2%80%93-Featured-Image.jpg"
+        name="abishek"
+        batch="B42WD"
+      />
+
+      <Details
+        access="✔"
+        img="https://easydrawingguides.com/wp-content/uploads/2019/08/how-to-draw-an-anime-boy-face-featured-image-1200.png"
+        name="hariharan"
+        batch="B41WD"
+      />
     </div>
   );
 }
 
-export default App;
+
+
+
+function Details(props) {
+  return (
+    <div className="card">
+      <img src={props.img} alt={props.name} />
+      <h2>
+        <span>{props.access}</span>
+        {props.name}
+      </h2>
+      <p>{props.batch}</p>
+    </div>
+  );
+}
+
